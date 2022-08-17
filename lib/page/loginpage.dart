e321c8070c68ae9a39b6ad0fb741ec63ac840f1f
nuageeeeee <106245123+nuageeeeee@users.noreply.github.com>
2022-08-17 23:30:17 +0200

    action back to main page

--- lib/page/loginpage.dart
+++ lib/page/loginpage.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import '../main.dart';
+import 'package:hotabank/main.dart';
 
 void main() {
-  runApp(loginPage());
+  runApp(const loginPage());
 }
 
+// ignore: camel_case_types
 class loginPage extends StatelessWidget {
   const loginPage({Key? key}) : super(key: key);
 
@@ -15,6 +16,14 @@ class loginPage extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: const Text('In working'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => const MyApp()));
+                },
+                icon: const Icon(Icons.abc))
+          ],
         ),
         body: const Center(
           child: Text('In working'),
