391ada7a5a55c82b9fc8c80333150e91f3076bdf
nuageeeeee <106245123+nuageeeeee@users.noreply.github.com>
2022-08-18 12:47:11 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,18 +16,10 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'HotaBank',
-                style: TextStyle(color: Colors.black),
-                textAlign: TextAlign.center,
-              )
-            ],
+          centerTitle: true,
+          title: const Text(
+            'tess',
+            style: TextStyle(color: Colors.black),
           ),
           leading: Builder(
             builder: (BuildContext context) {
@@ -36,7 +28,6 @@ class MyApp extends StatelessWidget {
                 color: Colors.black,
                 // alignment: const Alignment(-90, 0),
                 onPressed: () {
-                  // ignore: avoid_print
                   Navigator.push(
                     context,
                     MaterialPageRoute(builder: (context) => const loginPage()),
